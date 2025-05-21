c41d7aae51032083f291f2d4ce3bdfa400bfd3fd
Alina K <alina885@mail.ru>
2025-05-21 15:33:34 +0300

    added changes

--- src/test/java/demoqa/tests/DemoQaBookStoreTests.java
+++ src/test/java/demoqa/tests/DemoQaBookStoreTests.java
@@ -11,8 +11,9 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static demoqa.tests.TestData.isbn;
+import static demoqa.tests.TestData.userName;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,39 +24,35 @@ public class DemoQaBookStoreTests extends TestBase {
     @WithLogin
     @DisplayName("Удаление книги из профиля")
     void deleteBookTest() {
-        LoginResponseModel auth = step("Авторизация", () -> {
-            LoginResponseModel response = AuthApi.login();
-            assertThat(response.getToken()).isNotEmpty();
-            return response;
-        });
+        LoginResponseModel auth = step("Авторизация пользователя через API", AuthApi::login);
+        String token = auth.getToken();
+        String userId = auth.getUserId();
 
         step("Удаляем корзину пользователя", () ->
-                BookApi.deleteAllBooks(auth.getToken(), auth.getUserId())
+                BookApi.deleteAllBooks(token, userId)
         );
 
         step("Добавляем книгу через Api", () ->
-                BookApi.addBook(auth.getToken(), auth.getUserId(), TestData.isbn)
+                BookApi.addBook(token, userId, isbn)
         );
 
         step("Проверяем добавленную книгу через Api", () -> {
-            List<BookModel> books = BookApi.getUserBooks(auth.getToken(), auth.getUserId());
-            assertThat(books).extracting(BookModel::getIsbn).contains(TestData.isbn);
+            List<BookModel> books = BookApi.getUserBooks(token, userId);
+            assertThat(books).extracting(BookModel::getIsbn).contains(isbn);
         });
 
         step("Переходим на страницу профиля", () -> {
             open("/profile");
-            refresh();
-            $("#userName-value").shouldHave(text(auth.getUsername()));
-            $(".rt-tbody").shouldBe(visible);
+            $("#userName-value").shouldHave(text(userName));
         });
 
         step("Удаляем книгу через API", () ->
-                BookApi.deleteBook(auth.getToken(), auth.getUserId(), TestData.isbn)
+            BookApi.deleteBook(token, userId, isbn)
         );
 
         step("Проверка через API, что книга удалена", () -> {
-            List<BookModel> books = BookApi.getUserBooks(auth.getToken(), auth.getUserId());
-            assertThat(books).extracting(BookModel::getIsbn).doesNotContain(TestData.isbn);
+            List<BookModel> books = BookApi.getUserBooks(token, userId);
+            assertThat(books).extracting(BookModel::getIsbn).doesNotContain(isbn);
         });
     }
 }
